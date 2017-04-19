528f76f2841b38167ec0b918c9931c26d58624dc
Roberto Picchiottino <picchio@mbtlc.it>
2017-04-19 17:05:26 +0200

    Aggiunta secret_pub.php

---
+++ sito/etc/secret_pub.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Copiare il file in secret.php e modificarlo per le proprie esigenze.
+*/
+
+// configurazioni per accesso a db MySQL
+$db_user = "username";
+$db_host = "localhost";
+$db_pass = "password";
+$db_name = "nome_database";
+
+// altre configurazioni se necessarie o utili ma che siano segrete
+
+// non inserire nulla dopo questa riga!
+?>
