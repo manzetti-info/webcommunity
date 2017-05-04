b9497ac6490faa80dc7aee7879493d9c2ccf4f0e
Emanuele <info.cinquebit@gmail.com>
2017-05-04 13:02:55 +0000

    Includere solo etc/config.php

--- sito/picchio.php
+++ sito/picchio.php
@@ -1,9 +1,11 @@
 <?php
 
-	include ("sub/functions.php");
+	//include ("sub/functions.php"); // spostato in config.php
 	include ("etc/config.php");
-	get_head_page();
-	get_home();
-	get_end_page();
+	$page = "";
+	$page .= get_head_page();
 	
+	$page .= get_home();
+	$page .= get_end_page();
+	echo $page;
 ?>
